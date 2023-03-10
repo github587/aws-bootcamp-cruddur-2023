d9e11e8871dffeddf2ba40f8ee5adf6d2cb028bc
github587 <lakshmisr587@gmail.com>
2023-03-10 21:04:50 +0000

    fixed useractivities subsegment code

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -33,7 +33,6 @@ class UserActivities:
       }
       subsegment.put_metadata('key', dict, 'namespace')
       xray_recorder.end_subsegment()
-      xray_recorder.end_subsegment()
     finally:
       #  # Close the segment
       xray_recorder.end_subsegment()
